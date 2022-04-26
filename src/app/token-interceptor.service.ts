4d181eae92fb6351fa9a0d900a3acf11ab6953b9
Ankit saxena <ankits.gits@gmail.com>
2022-04-26 22:47:51 +0530

    unauthorized request redirected to login

--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,HttpHeaders} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable,of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+    private router: Router) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if(req.url.indexOf('/oauth') == -1 && req.url.indexOf('/saveuser') == -1 && req.url.indexOf('/verifyemail') == -1 && req.url.indexOf('/resetpassword') == -1 && req.url.indexOf('/changepassword') == -1 && req.url.indexOf('/emailExists') == -1){
+      if(JSON.parse(window.sessionStorage.getItem('token')) == undefined){
+        this.router.navigate(['login']);
+      return of(undefined);
+      }
+      else{
       req= req.clone({
         setHeaders: {Authorization: `Bearer ${JSON.parse(window.sessionStorage.getItem('token')).access_token}`}
        })
       return next.handle(req)
+      }
       }
       else{
         return next.handle(req)
